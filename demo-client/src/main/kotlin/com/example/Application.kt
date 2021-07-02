7152283f9b224c5bb9f420c13ce1b9d767b2d060
Bruno Eduardo <bruno@beduardo.com>
2021-07-02 00:19:41 -0300

    add saveUserStream

--- demo-client/src/main/kotlin/com/example/Application.kt
+++ demo-client/src/main/kotlin/com/example/Application.kt
@@ -5,8 +5,9 @@ import io.micronaut.runtime.Micronaut.*
 suspend fun main(args: Array<String>) {
 
 	println("Hello World!")
-	//val demoService = DemoService()
-	//demoService.withWaitForReady().saveUser()
+	val demoService = DemoService()
+	// demoService.saveUser()
+	demoService.saveUserStream()
 
 	build()
 	    .args(*args)
--- demo-client/src/main/kotlin/com/example/DemoService.kt
+++ demo-client/src/main/kotlin/com/example/DemoService.kt
@@ -2,6 +2,10 @@ package com.example
 
 import io.grpc.Channel
 import io.grpc.ManagedChannelBuilder
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 
 class DemoService {
 
@@ -17,7 +21,45 @@ class DemoService {
         val saveUserResponse = demoServerStub.saveUser(saveUserRequest)
 
         println("Registered user with id = " + saveUserResponse.id)
-        println("Registered user with id = " + saveUserResponse.id)
+    }
+
+    suspend fun saveUserStream() {
+        val demoServerStub = createStub()
+
+        val requests = generateOutgoingRequests()
+
+        demoServerStub.saveUserStream(requests).collect { response ->
+            println("Response: " + response.id)
+        }
+    }
+
+    private fun generateOutgoingRequests(): Flow<SaveUserRequest> = flow {
+
+        val request1 = SaveUserRequest.newBuilder()
+            .setName("John")
+            .setLastName("Doe")
+            .setDocument("05262438594")
+            .build()
+
+        val request2 = SaveUserRequest.newBuilder()
+            .setName("John")
+            .setLastName("Doe 2")
+            .setDocument("07262438594")
+            .build()
+
+        val request3 = SaveUserRequest.newBuilder()
+            .setName("Jane")
+            .setLastName("Doe")
+            .setDocument("09262438594")
+            .build()
+
+        val requests = listOf(request1, request2, request3)
+
+        for (request in requests) {
+            println("Request: " + request.name)
+            emit(request)
+            delay(5000)
+        }
     }
 
     private fun createStub(): DemoServerServiceGrpcKt.DemoServerServiceCoroutineStub {
